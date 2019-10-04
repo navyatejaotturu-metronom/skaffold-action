2808290647a2cc89e489615ca6232632fd0bce01
anarcher <anarcher@gmail.com>
2019-10-04 23:04:14 +0900

    docker login

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
+if [ -n "${INPUT_DOCKER_PASSWORD}" ]; then
+    sh -c "docker login -u $INPUT_DOCKER_USERNAME -p $INPUT_DOCKER_PASSWORD $INPUT_REGISTRY"
+fi
+
 if [ -z "${INPUT_SKAFFOLD}" ]; then
     echo "skaffold args is empty. Please set with.skaffold."
 fi 
