390fd473789a5a099e84c50a4578a4d7dfc68b1f
anarcher <anarcher@gmail.com>
2019-10-06 03:37:51 +0900

    execute skaffold when skaffold input is set only

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,20 +5,21 @@ if [ -n "${INPUT_DOCKER_PASSWORD}" ]; then
     sh -c "echo ${INPUT_DOCKER_PASSWORD} | docker login -u ${INPUT_DOCKER_USERNAME} --password-stdin ${INPUT_DOCKER_REGISTRY}"
 fi
 
-if [ -z "${INPUT_SKAFFOLD}" ]; then
-    echo "skaffold args is empty. Please set with.skaffold."
-fi 
+#if [ -z "${INPUT_SKAFFOLD}" ]; then
+#    echo "skaffold args is empty. Please set with.skaffold."
+#fi 
 
 if [ -n "${IMAGE_TAG}" ]; then 
-    echo "GITHUB_REF: ${GITHUB_REF}"
     echo "IMAGE_TAG: ${IMAGE_TAG}"
     IMAGE_TAG=$(echo ${IMAGE_TAG} | sed -e "s/refs\/heads\///g")
     IMAGE_TAG=$(echo ${IMAGE_TAG} | sed -e "s/refs\/tags\///g" | sed -e "s/\//-/g") 
     echo "IMAGE_TAG: ${IMAGE_TAG}"
 fi 
 
-echo "[SKAFFOLD]"
-sh -c "skaffold ${INPUT_SKAFFOLD}"
+if [ -n "${INPUT_SKAFFOLD}" ]; then
+    echo "[SKAFFOLD]"
+    sh -c "skaffold ${INPUT_SKAFFOLD}"
+fi
 
 if [ -n "${INPUT_KUSTOMIZE}" ]; then
     echo "[KUSTOMIZE]"
