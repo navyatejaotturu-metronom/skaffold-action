130cf0fcf735cba4da0e148820f43be56a9271cc
Navya Teja Otturu <navya.teja@metronom-external.com>
2020-04-22 19:14:22 +0530

    Exit the script if error occurs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh -l
 
+function checkExitStatus() {
+  if [ "$1" -gt "0" ]; then
+    exit $1
+  if
+}
 if [ -n "${INPUT_DOCKER_PASSWORD}" ]; then
     echo "[DOCKER_LOGIN]"
     sh -c "echo ${INPUT_DOCKER_PASSWORD} | docker login -u ${INPUT_DOCKER_USERNAME} --password-stdin ${INPUT_DOCKER_REGISTRY}"
+    checkExitStatus $?
 fi
-
-#if [ -z "${INPUT_SKAFFOLD}" ]; then
-#    echo "skaffold args is empty. Please set with.skaffold."
-#fi 
 
 if [ -n "${IMAGE_TAG}" ]; then 
     echo "IMAGE_TAG: ${IMAGE_TAG}"
@@ -19,13 +21,16 @@ fi
 if [ -n "${INPUT_SKAFFOLD}" ]; then
     echo "[SKAFFOLD]"
     sh -c "skaffold ${INPUT_SKAFFOLD}"
+    checkExitStatus $?
 fi
 
 if [ -n "${INPUT_KUSTOMIZE}" ]; then
     echo "[KUSTOMIZE]"
     if [ -n "${INPUT_KUSTOMIZE_PATH}" ]; then 
         sh -c "cd ${INPUT_KUSTOMIZE_PATH}; kustomize ${INPUT_KUSTOMIZE}"
+        checkExitStatus $?
     else
         sh -c "kustomize ${INPUT_KUSTOMIZE}"
+        checkExitStatus $?
     fi
 fi 
